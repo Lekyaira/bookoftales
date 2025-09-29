c6196673d9828a1717eda62c7f86bdccd422b2b9
Ryan Anderson <anderson.ryan.w@live.com>
2025-09-29 18:21:44 -0500

    Set up 'bookoftales' db and sql scripts

--- db/shell.nix
+++ db/shell.nix
@@ -11,12 +11,11 @@ pkgs.mkShell {
 	# Postgres
 	PGDATA = ".dbdata";
 	# Edit this to set the Postgres database name
-	BLOGDB = "blog";
-	USERDB = "users";
+	BOOKDB = "bookoftales";
 
 	shellHook = ''
 		#### Utils ####
-		BINDIR=${pd}/.nix/bin 
+		BINDIR=${pd}/scripts
 		export PATH=$PATH:$BINDIR
 
 		GREEN='\033[0;32m'
@@ -26,8 +25,7 @@ pkgs.mkShell {
 		# Create data directory if it does not exist and initialize it
 		[ ! -d .dbdata ] && mkdir .dbdata && initdb
 		# Initialize the database if it does not exist
-		pg_ctl -l logfile -o "--unix_socket_directories='$PWD'" start && psql -h $PWD -tAl | cut -d '|' -f 1 | grep -qx "$PGDB" || createdb -h "$PWD" "$BLOGDB"
-		psql -h $PWD -tAl | cut -d '|' -f 1 | grep -qx "$PGDB" || createdb -h "$PWD" "$USERDB"
+		pg_ctl -l logfile -o "--unix_socket_directories='$PWD'" start && psql -h $PWD -tAl | cut -d '|' -f 1 | grep -qx "$BOOKDB" || createdb -h "$PWD" "$BOOKDB"
 		[ -f "$PGDATA/postmaster.pid" ] && pg_ctl stop
 		
 		printf "$GREEN\nUse 'start' to start Postgres server.\nUse 'stop' to stop Postgres server.\nUse 'sql' to start the Postgres cli.\n\n$NC"
